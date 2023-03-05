a6941c573e601cd79c47d34c034f452e46508e01
meroglum <45095695+meroglum@users.noreply.github.com>
2023-03-05 03:59:04 +0300

    First commiy

---
+++ C# Basics01/ConsoleApp1/27.12.2022.cs
@@ -0,0 +1,258 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        int abc;
+        int yas = 29;
+        int değer = 2147483547;
+
+        int a, b, c;
+        //int a = 1, b = 2, c = 3;
+
+        int x = new int();
+        Console.WriteLine(x); ///Defalut int value is 0
+                              ///If it would be bool the default value is false
+
+        int? y; ///It is nullable, can be used when collecting data from database
+
+        /// y = getData();
+       
+        ///Teknik olarak ayırmak için sonuna karakter koyuluyor
+        double d1 = 1.20; //Double için souna karakter koymaya gerek yok
+        float f1 = 2.1F; //Float için F
+        decimal dec1 = 4.3M; //decimal için souna M koyulmalı 
+
+        float ff1;
+        ff1 = 2.3F;
+
+        char ch = 'A'; //Tek tırnak olmalı çift tırnak String için kullanılır
+        char ch1 = (char)54;
+        Console.WriteLine(ch1);
+        String adx = "Muhammed";
+
+        /// Kod çok kalabalık olduğu zaman region kullanılabilir 
+        #region önemli
+        //CTRL+K+C yorum satırına alır
+        ///açıklamalar
+        //Console.Write(ch);
+        //Console.Write(ch);
+        //Console.Write(ch);
+        //Console.Write(ch);
+        //Console.Write(ch);
+        #endregion
+
+        #region önemli2
+        //CTRL+K+C yorum satırına alır
+        ///açıklamalar
+        //Console.Write(ch);
+        //Console.Write(ch);
+        //Console.Write(ch);
+        //Console.Write(ch);
+        //Console.Write(ch);
+        #endregion
+
+        #region önemli3
+        //CTRL+K+C yorum satırına alır
+        ///açıklamalar
+        //Console.Write(ch);
+        //Console.Write(ch);
+        //Console.Write(ch);
+        //Console.Write(ch);
+        //Console.Write(ch);
+        #endregion
+
+        int abc1 = 0;
+        double abc2 = new double();
+        bool abc3 = default(bool); ///bool tipinin defult değerini değişken olarak atar
+        System.Int64 xxx = 3321231223131231312; ///32 bit int değeri yetersiz geldiğinde kullan
+        int abc4 = default(int);///gets the defult int
+
+        #region bir dairenin alanını hesaplama
+        double radius;
+        double area;
+        radius = 10.0;
+        area = radius * radius * 3.1418;
+        Console.WriteLine("Alan : " +area);
+
+        ///aynı kod çok daha kısa şekilde yapılabilir
+        double radius2 = 10.0; double area2 = radius2 * radius2 * 3.1418;
+        Console.WriteLine("Kısa kodala hesaplanmış Alan : " +area2);
+        #endregion
+
+        #region Hesaplama
+        decimal amount;
+        decimal rate_of_return;
+        int years;
+        amount = 1000.0m;
+        rate_of_return = 0.07m;
+        years = 10;
+        Console.WriteLine("Original Investment: $" + amount);
+        Console.WriteLine("Rate of return : " +rate_of_return);
+        Console.WriteLine("Over " + years+" years");
+        #endregion
+
+        #region indirimli fiyat hesaplama
+        decimal price, discount, discounted_price;
+        ///Indirimli fiyat hesapla
+        price = 19.95m;
+        discount = 0.15m;
+        discounted_price = price - (price * discount); ///indirimi hesapla
+        Console.WriteLine("Discounted Price : $"+discounted_price);
+        #endregion
+
+        static decimal IndirimHesapla(decimal price, decimal discount)
+        {
+            if(price >0 && discount > 0)
+            {
+                return price - (price * discount);
+            }
+            return 0; ///Hata fırlatma kullanılabilir
+        }
+
+        #region String manupülasyon
+        String ad, soyad, email, fb_profil, sonuc, ayirac = "********";
+        ad = "Muhammed";
+        soyad = "EROĞLU";
+        email = "muhammedf.eroglu@gmail.com";
+        fb_profil = "Yok";
+
+        // Metin Birleştirme (String Concat)
+
+        sonuc = ayirac + "\n" +
+            "Ad : " + ad + "\n" +
+            "Soyad : " + soyad + "\n" +
+            "Email : " + email + "\n" +
+            "Facebook : " + fb_profil + "\n" +
+            ayirac;
+
+        Console.WriteLine(sonuc);
+
+
+        ///Bunu yapmanın diğer yolu ise String builder kullanmak
+        ///String builder ile performans daha iyi olur 
+        ///yukarıdaki karmaşık ve ayrıca yukarıdaki durum performans düşüklüğüne 
+        ///neden olur 
+        ///String builder performansı çok fazla yükseltir
+        ///string sayısı arttıkça preformans farkı gözle görülür olur
+        StringBuilder builder = new StringBuilder();
+        builder.Append("Ad : " + ad);
+        builder.Append(Environment.NewLine);
+        builder.Append("Soyad : ");
+        builder.Append(soyad);
+        Console.WriteLine(builder.ToString());
+        #endregion
+
+        #region input alma
+        Console.Write("Ad :");
+        ad = Console.ReadLine();
+        Console.Write("Soyad :");
+        soyad = Console.ReadLine();
+        Console.Write("E mail :");
+        email = Console.ReadLine();
+        Console.Write("facebook :");
+        fb_profil = Console.ReadLine();
+       
+
+        // Metin Birleştirme (String Concat)
+
+        sonuc = ayirac + "\n" +
+            "Ad : " + ad + "\n" +
+            "Soyad : " + soyad + "\n" +
+            "Email : " + email + "\n" +
+            "Facebook : " + fb_profil + "\n" +
+            ayirac;
+
+        Console.WriteLine(sonuc);
+        String s2 = String.Empty;
+
+
+        #endregion
+
+        // Alt satıra inme
+        // "\n"
+        // environment variable
+        // ASCI alt satıra inme karakteri
+
+        #region nullable types
+        int? num = null;
+        double? numberdouble = null;
+        bool? cinsiyet = null;
+
+        //Eğer var anahtar kelimesi kullanarak bir değişken oluşturmak
+        //isteniyor ve değeri belli değilse, aşağıdaki gibi tanımlanabilir.
+        var isimX = (string)null;
+
+        #endregion
+
+        #region daire çevre ve alan
+        // pi sayısını tanımlayalım
+        const double pi = 3.14159;
+
+        //Değişkenleri tanımlayalım
+        double alan, cevre, yaricap;
+        String yaricapGir;
+        Console.WriteLine("Dairenin yarıçapını giriniz");
+        yaricapGir = Console.ReadLine();
+        yaricap = Convert.ToDouble(yaricapGir);
+        cevre = 2 * pi * yaricap;
+        alan = pi * Math.Pow(yaricap, 2);
+        Console.WriteLine("Dairenin alanı = {0}",alan); //{0} is placeholder
+        Console.WriteLine("Dairenin çevresi = {0}",cevre); //{0} is placeholder
+        Console.ReadKey(); ///Konsol hemen kapanmasın diye
+        #endregion
+
+        #region TÜR DÖNÜŞÜMÜ
+        //Tür dönüşümü: Bilinçli ve bilinçsiz kavramları
+        //Tür dönüşümü: Type Casting
+        //Tür dönüşümü: Convert sınıfı ile tür dönüşümü
+        //Tür dönüşümü: (cast,convert,parse)
+
+        //büyükten küçüğe
+
+        double d = 12.3;
+        Console.WriteLine(d);
+        int i = Convert.ToInt32(d);
+        Console.WriteLine(i);
+
+        //küçükten büyüğe
+        int i1 = 123456789;
+        double d12 = 123456789.1;
+        d12 = i1;
+
+        ///Arada yazamadıklarım var onları fotoğraftan geçir
+
+        public enum dayOfTheWeek
+    {
+        Sunday = 0,
+        Monday = 1,
+        Tuesday = 2,
+        Wednesday = 3,
+        Thursday = 4,
+        Friday = 5,
+    Saturday = 6,
+    }
+        #endregion
+
+        #region Kapsayıcı
+
+        #endregion
+
+        #region var kullanımı
+
+        #endregion
+
+        #region obj ve var
+
+        #endregion
+
+        #region obj ve var
+
+        #endregion
+    }
+}
+ 
---
+++ C# Basics02/ConsoleApp2022.12.28/Program.cs
@@ -0,0 +1,470 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+/// <summary>
+/// Muhammed EROGLU C# Notes
+/// </summary>
+class Program
+{
+    public static void Main(string[] args)
+    {
+        /*
+            
+                +          Toplama
+                -          Çıkarma
+                *          Çarpma
+                /          Bölme
+                %          Bölümden Kalan
+                ++         Arttırma
+                --         Eksiltme
+
+                + , ‐ , * , /, ++ , -- ve % aritmetik  operatörlerdir.
+                /, *, + ve – operatörleri matematiksel işlemler yapar.
+                % operatörü ise bölümden sonra kalanı bulmak yani "mod" almak için kullanılır.
+                ++ ve -- işlemin uygulandığı sayıyı 1 arttırır veya 1 azaltır.
+                *, /, % operatörleri aynı satırdaysa eşit önceliğe sahiptir. + ve – operatörleri de aynı satırdaysa eşit önceliklidir fakat  *, /, % operatörlerinin + ve – operatörlerine göre önceliği vardır.
+     */
+       
+        int x = 100;
+        int y = 5;
+
+        Console.WriteLine("x + y = {0}", x + y);
+        Console.WriteLine("x - y = {0}", x - y);
+        Console.WriteLine("x * y = {0}", x * y);
+        Console.WriteLine("x / y = {0}", x / y);
+
+        int toplam = 0, fark = 0, carpim = 0, modulo = 0;
+        float bolum = 0; //0f de yazılabilir
+        int sayi1 = 10, sayi2 = 2;
+        toplam = sayi1 + sayi2;
+        fark = sayi1 + sayi2;
+        carpim = sayi1 * sayi2;
+        bolum = sayi1 / sayi2;
+        modulo = 3 % sayi2;         //  3/2 nin kalanı (1)
+
+        Console.WriteLine("sayı1 = {0}, sayı2 = {1}", sayi1, sayi2);
+        Console.WriteLine(); // Komsolda satır boşluğu bırakmak için kullanılır.
+        Console.WriteLine("{0} + {1} = {2}", sayi1, sayi2, toplam); // {1} : sayi2 indexe göre yazdır.
+        Console.WriteLine("{0} - {1} = {2}", sayi1, sayi2, fark);
+        Console.WriteLine("{0} * {1} = {2}", sayi1, sayi2, carpim);
+        Console.WriteLine("{0} / {1} = {2}", sayi1, sayi2, bolum);
+        Console.WriteLine();
+        Console.WriteLine("3 sayısı {0} ile bölününce {1} kalır.", sayi2, modulo);
+
+        /*
+            Çok Boyutlu Dizi Türleri
+                - Düzenli Çok Boyutlu Diziler
+                    - İki Boyutlu Diziler
+                    - Üç Boyutlu Diziler
+                - Düzensiz (Jagged) Çok Boyutlu Diziler
+            https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/arrays/multidimensional-arrays
+            https://www.w3schools.com/cs/cs_arrays_multi.php
+         */
+
+        //int x = 100;
+        //        int y = 5;
+
+        //        Console.WriteLine("x + y = {0}", x + y);
+        //        Console.WriteLine("x - y = {0}", x - y);
+        //        Console.WriteLine("x * y = {0}", x * y);
+        //        Console.WriteLine("x / y = {0}", x / y);
+
+        //int toplam = 0, fark = 0, carpim = 0, modulo = 0;
+        //        float bolum = 0;
+        //        int sayi1 = 10, sayi2 = 2;
+        //        toplam = sayi1 + sayi2;
+        //        fark = sayi1 + sayi2;
+        //        carpim = sayi1 * sayi2;
+        //        bolum = sayi1 / sayi2;
+        //        modulo = 3 % sayi2;         //  3/2 nin kalanı
+
+        //        Console.WriteLine("sayı1 = {0}, sayı2 = {1}", sayi1, sayi2);
+        //        Console.WriteLine();
+        //        Console.WriteLine("{0} + {1} = {2}", sayi1, sayi2, toplam);
+        //        Console.WriteLine("{0} - {1} = {2}", sayi1, sayi2, fark);
+        //        Console.WriteLine("{0} * {1} = {2}", sayi1, sayi2, carpim);
+        //        Console.WriteLine("{0} / {1} = {2}", sayi1, sayi2, bolum);
+        //        Console.WriteLine();
+        //        Console.WriteLine("3 sayısı {0} ile bölününce {1} kalır.", sayi2, modulo);
+
+        // ++  ve  --
+
+        //int x = 1;
+        //Console.WriteLine(x++);
+        //Console.WriteLine(++x);
+
+        //int xx = 1;
+        //xx = xx + 1;
+        //xx += 1;
+        //x++;
+
+        //int x = 100;
+        //        int y = 5;
+        
+        //  Console.WriteLine("x + y = {0}", x + y);
+        //  Console.WriteLine("x - y = {0}", x - y);
+        //  Console.WriteLine("x * y = {0}", x * y);
+        //  Console.WriteLine("x / y = {0}", x / y);
+
+      
+        //int toplam = 0, fark = 0, carpim = 0, modulo = 0;
+        //  float bolum = 0;
+        //  int sayi1 = 10, sayi2 = 2;
+        //  toplam = sayi1 + sayi2;
+        //  fark = sayi1 + sayi2;
+        //  carpim = sayi1 * sayi2;
+        //  bolum = sayi1 / sayi2;
+        //  modulo = 3 % sayi2;         //  3/2 nin kalanı
+
+        Console.WriteLine("sayı1 = {0}, sayı2 = {1}", sayi1, sayi2);
+        Console.WriteLine();
+        Console.WriteLine("{0} + {1} = {2}", sayi1, sayi2, toplam);
+        Console.WriteLine("{0} - {1} = {2}", sayi1, sayi2, fark);
+        Console.WriteLine("{0} * {1} = {2}", sayi1, sayi2, carpim);
+        Console.WriteLine("{0} / {1} = {2}", sayi1, sayi2, bolum);
+        Console.WriteLine();
+        Console.WriteLine("3 sayısı {0} ile bölününce {1} kalır.", sayi2, modulo);
+
+
+        // ++  ve  --
+
+        //int x = 1;
+        //Console.WriteLine(x++);
+        //Console.WriteLine(++x);
+
+        //int xx = 1;
+        //xx = xx + 1;
+        //xx += 1;
+        //x++;
+
+         /*
+         == Operatörü    : Eşittir anlamındadır.
+         != Operatörü    : Eşit değil (farklı) anlamındadır.
+         > Operatörü     : Büyüktür anlamındadır.Bu operatör string işlemlere uygulanmaz.
+         < Operatörü     : Küçüktür anlamındadır.Bu operatör string işlemlere uygulanmaz.
+         >= Operatörü    : Büyük veya eşit anlamındadır. Bu operatör string işlemlere uygulanmaz.
+         <= Operatörü    : Küçük veya eşit anlamındadır.Bu operatör string işlemlere uygulanmaz.
+         */
+
+        /*
+        (veya) operatörü      iki değerden birisi doğru ise doğru, ikisi de yanlış ise yanlış değerini döndürür ayrıca önceliği en az olan mantıksal operatördür.
+        &&(ve) operatörü        iki değerin ikisi de doğru ise doğru, en az birisi yanlış ise yanlış değerini döndürür.
+        &&(ve), (veya) operatörleri aritmetik, karşılaştırma ve bitsel operatörlere göre düşük önceliklidir fakat kendi aralarındaki öncelik sıralaması &&(ve), ||(veya) şeklindedir.
+        !(değil) operatörü      aritmetik operatörlerden (++)arttırma ve (--)eksiltme operatörleri ile eşit önceliklidir.
+        */
+
+   
+        //int x = 100;
+        //        int y = 5;
+        
+        //  Console.WriteLine("x + y = {0}", x + y);
+        //  Console.WriteLine("x - y = {0}", x - y);
+        //  Console.WriteLine("x * y = {0}", x * y);
+        //  Console.WriteLine("x / y = {0}", x / y);
+
+
+        //int toplam = 0, fark = 0, carpim = 0, modulo = 0;
+        //  float bolum = 0;
+        //  int sayi1 = 10, sayi2 = 2;
+        //  toplam = sayi1 + sayi2;
+        //  fark = sayi1 + sayi2;
+        //  carpim = sayi1 * sayi2;
+        //  bolum = sayi1 / sayi2;
+        //  modulo = 3 % sayi2;         //  3/2 nin kalanı
+
+        Console.WriteLine("sayı1 = {0}, sayı2 = {1}", sayi1, sayi2);
+        Console.WriteLine();
+        Console.WriteLine("{0} + {1} = {2}", sayi1, sayi2, toplam);
+        Console.WriteLine("{0} - {1} = {2}", sayi1, sayi2, fark);
+        Console.WriteLine("{0} * {1} = {2}", sayi1, sayi2, carpim);
+        Console.WriteLine("{0} / {1} = {2}", sayi1, sayi2, bolum);
+        Console.WriteLine();
+        Console.WriteLine("3 sayısı {0} ile bölününce {1} kalır.", sayi2, modulo);
+
+       
+        // ++  ve  --
+
+        //int x = 1;
+        //Console.WriteLine(x++);
+        //Console.WriteLine(++x);
+
+        //int xx = 1;
+        //xx = xx + 1;
+        //xx += 1;
+        //x++;
+
+        // Üçlü Operatör - Ternary Operator
+
+        // operand1 ? operand2 : operand2
+
+        string mesaj = string.Empty;
+        bool aktifMi = true;
+        mesaj = aktifMi ? "Evet" : "Hayır";
+        //mesaj = aktifMi ? function1() : function2();
+        Console.WriteLine("Aktif mi : " + mesaj);
+
+        // Bitsel operatörler
+
+        /*
+            Bitsel operatörler &(bitsel ve), ~(bitsel değil), |(bitsel veya), ^(bitsel özel veya) operatörleridir. 
+            Sayıların kendileri yerine bitlerini kullanan operatörlerdir.
+            
+            ~ : Tilde
+            &(ve) işlemi karşılıklı basamaklardan her ikisi de 1 ise ilgili basamağında 1 tutar, diğer durumlarda 0 tutar.
+            ~(değil) operatörü operandının her basamağını tersleştirip tutar.
+            |(veya) işlemi karşılıklı basamaklarından herhangi birisi 1 ise ilgili basamağında 1 tutar, diğer durumlarda 0 tutar.
+            ^(özel veya) işlemi karşılıklı basamakları farklıysa ilgili basamağında 1 tutar, diğer durumlarda 0 tutar.
+            Bitsel operatörler aritmetik operatörler ve karşılaştırma operatörlerinden düşük önceliklidir fakat bu operatörler aynı satırda olduklarında aralarındaki öncelik sıralaması &(bitsel ve), ^ (bitsel özel veya), |(bitsel veya) şeklindedir.
+
+            Console.WriteLine(9<<2); // 36 Olarak Ekrana Çıktı Gösterir.
+Console.    WriteLine(9>>2); // 2 Olarak Ekrana Çıktı Gösterir.
+
+            İlk örneğimiz üzerinde girdiğimiz 9 değerinin binary karşılığı 1001‘e karşılık gelir.
+            Bu sayıyı 2 bit değerinde sağa kaydırdığımız zaman yeni elde edeceğimiz değer 0010 olur
+            ve bu değerin karşılığı 2’ye denk gelir. Aynı şekilde işlemi 2 bit sola kaydıracağımız zaman
+            ise değerimiz 100100 olur. Bu bitsel sonucun değeri de 36’ya karşılık gelmektedir.
+
+            Açıklamadan anlaşılacağı üzere burada değerlerin kendileri değil binary karşılıkları işleme tabi tutulur.
+         */
+
+        /*
+            () tür dönüştürme operatörü:    Tür değiştirmek için kullanılan bir operatördür.   (değiştirilecek tür) değişken_yada_sabit
+            [] operatörü:                   Dizilerde elemanın dizinini belirtmek için kullanılır.
+            + ve – operatörü:               Bir değişkenin pozitif veya negatif olmasını sağlar. Ayrıca + operatörü iki string(katar)’i birleştirmekte de kullanılır. Kullanımı şu şekildedir;
+            typeof operatörü:               Herhangi bir değişken türünün CTS (Common Type System - Ortak Tip Sistemi) karşılığını type(tip) olarak tutar.
+            sizeof operatörü:               Temel türlerin ve yapıların bellekte ne kadar alan kapladıklarını verir. "sizeof" operatörü sınıflar için kullanılamaz ancak tanımlanacak yapılar için kullanılabilir.
+            new operatörü:                  Yeni bir nesne oluşturmak için kullanılan operatörlerdir.
+         */
+
+        #region İki Boyutlu Diziler
+
+        //int[,] ikiBoyut;
+        //int[,] ikiBoyut = new int[4,2];
+        //int[,] ikiBoyut = {
+        //                { 1, 2 },
+        //                { 3, 4 },
+        //                { 5, 6 },
+        //                { 7, 8 }
+        //              };
+
+        //Console.WriteLine(ikiBoyut.Length);
+
+        //for (int i = 0; i < 4; i++)
+        //{
+        //    for (int j = 0; j < 2; j++)
+        //    {
+        //        Console.WriteLine(ikiBoyut[i, j]);
+        //    }
+        //}
+
+        //Console.WriteLine("---------------->");
+
+        //ikiBoyut[0, 0] = 10;
+        //ikiBoyut[0, 1] = 20;
+        //ikiBoyut[1, 0] = 30;
+        //ikiBoyut[1, 1] = 40;
+        //ikiBoyut[2, 0] = 50;
+        //ikiBoyut[2, 1] = 60;
+        //ikiBoyut[3, 0] = 70;
+        //ikiBoyut[3, 1] = 80;
+
+        // for (int i = 0; i < 4; i++)
+        // {
+        //    for (int j = 0; j < 2; j++)
+        //    {
+        //        Console.WriteLine(ikiBoyut[i, j]);
+        //    }
+        // }
+
+        #endregion
+
+        #region Üç Boyutlu Diziler
+
+        //int[,,] ucBoyut;
+        //int[,,] ucBoyut = new int[4, 2, 2];
+        //int[,,] ucBoyut = {
+        //                           {{1,2},{3,4}},
+        //                           {{5,6},{7,8}},
+        //                           {{9,10},{11,12}},
+        //                           {{13,14},{15,16}}
+        //                       };
+
+        //for (int i = 0; i < 4; i++)
+        //{
+        //    for (int j = 0; j < 2; j++)
+        //    {
+        //        for (int k = 0; k < 2; k++)
+        //        {
+        //            Console.WriteLine(ucBoyut[i, j, k]);
+        //        }
+        //    }
+        //}
+
+        //ucBoyut[0, 0, 0] = 10;
+        //ucBoyut[0, 0, 1] = 20;
+        //ucBoyut[0, 1, 0] = 30;
+        //ucBoyut[0, 1, 1] = 40;
+        //ucBoyut[1, 0, 0] = 50;
+        //ucBoyut[1, 0, 1] = 60;
+
+        #endregion
+
+        #region Düzensiz(Jagged) Diziler
+
+        //int[][] jagged =
+        //{
+        //    new int[] { 1, 2, 3, 4, 5, 6 },
+        //    new int[] { 1, 2 },
+        //    new int[] { 1, 2, 3, 4 },
+        //    new int[] { 1 }
+        //};
+
+        //Console.WriteLine(jagged.Length);
+
+        //foreach (var jag in jagged)
+        //{
+        //    foreach (var ged in jag)
+        //    {
+        //        Console.Write(ged);
+        //    }
+        //    Console.WriteLine();
+        //}
+
+        #endregion
+
+        //  int x = 100;
+        //  int y = 5;
+
+        //  Console.WriteLine("x + y = {0}", x + y);
+        //  Console.WriteLine("x - y = {0}", x - y);
+        //  Console.WriteLine("x * y = {0}", x * y);
+        //  Console.WriteLine("x / y = {0}", x / y);
+
+        //  int toplam = 0, fark = 0, carpim = 0, modulo = 0;
+        //  float bolum = 0;
+        //  int sayi1 = 10, sayi2 = 2;
+        //  toplam = sayi1 + sayi2;
+        //  fark = sayi1 + sayi2;
+        //  carpim = sayi1 * sayi2;
+        //  bolum = sayi1 / sayi2;
+        //  modulo = 3 % sayi2;         //  3/2 nin kalanı
+
+        //  Console.WriteLine("sayı1 = {0}, sayı2 = {1}", sayi1, sayi2);
+        //  Console.WriteLine();
+        //  Console.WriteLine("{0} + {1} = {2}", sayi1, sayi2, toplam);
+        //  Console.WriteLine("{0} - {1} = {2}", sayi1, sayi2, fark);
+        //  Console.WriteLine("{0} * {1} = {2}", sayi1, sayi2, carpim);
+        //  Console.WriteLine("{0} / {1} = {2}", sayi1, sayi2, bolum);
+        //  Console.WriteLine();
+        //  Console.WriteLine("3 sayısı {0} ile bölününce {1} kalır.", sayi2, modulo);
+
+        //  static void Main(string[] args)
+        //  {
+        // Akış Kontrolü : if ve switch (switch case)
+        // 1
+        //   if (true)
+        //    Console.WriteLine("adada");
+        // 2
+        //   bool isValid = true;
+        //   if (isValid)
+        //    Console.WriteLine("Doğru");
+        //   else
+        //    Console.WriteLine("Yanlış"); 
+        //    Console.WriteLine("Yanlış mı");     // ?
+        // 3
+        //   bool isValid = true;
+        //  if (isValid)
+        //    Console.WriteLine("Doğru");
+        //   else
+        //   {
+        //    Console.WriteLine("Yanlış");
+        //    Console.WriteLine("Yanlış mı");
+        //  }
+        // 4
+        //  int x = 1, y = 3;
+        //  if (x > y)
+        // {
+        //}
+        //else
+        //  {
+        //  }
+        // 5
+        //  int x = 1, y = 3;
+        // if (x <= y)
+        //  {
+        //  }
+        //  else if (x == y)
+        // {
+        //  }
+        // else if (x >= y)
+        //  {
+        //  }
+        //  else
+        // {
+        //  }
+        // 6
+        //  if (true)
+        //  {
+        // if (true)
+        // {
+        //  if (true)
+        //  {
+        //  }
+        //    }
+         //}
+         //else
+         //{
+        //}
+        // 7
+        //int sayi1 = 8, sayi2 = 9, sonuc = 0;
+        //if (sayi1 < sayi2)
+        //{
+        //    sayi1++;
+        //    //sonuc = sayi1++;
+        //    Console.WriteLine("Küçüktür.");
+        //}
+        //else if (sayi1 == sayi2)
+        //{
+        //    Console.WriteLine("Eşittir.");
+        //}
+        //else
+        //{
+        //    sonuc = sayi1 + sayi2;
+        //    Console.WriteLine("Büyüktür.");
+        //}
+        //Console.WriteLine("Sonuç : " + sonuc);
+        
+        // 8
+
+        //  byte ay = Convert.ToByte(Console.ReadLine());
+
+        //  if (ay == 1)
+        //      Console.WriteLine("Ocak");
+        //  else if (ay == 2)
+        //      Console.WriteLine("Şubat");
+        //  else if (ay == 3)
+        //      Console.WriteLine("Mart");
+        //  else if (ay == 4)
+        //      Console.WriteLine("Nisan");
+        //  else if (ay == 5)
+        //      Console.WriteLine("Mayıs");
+        //  else if (ay == 6)
+        //      Console.WriteLine("Haziran");
+        //  else if (ay == 7)
+        //      Console.WriteLine("Temmuz");
+        //  else if (ay == 8)
+        //      Console.WriteLine("Ağustos");
+        //  else if (ay == 9)
+        //      Console.WriteLine("Eylül");
+        //  else if (ay == 10)
+        //      Console.WriteLine("Ekim");
+        //  else if (ay == 11)
+        //      Console.WriteLine("Kasım");
+        //  else if (ay == 12)
+        //      Console.WriteLine("Aralık");
+        //  else
+        //      Console.WriteLine("1-12 arası sayı girmelisiniz");
+        //  }
+    }
+}
