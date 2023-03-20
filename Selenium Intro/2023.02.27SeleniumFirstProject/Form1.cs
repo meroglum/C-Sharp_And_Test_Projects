a483f852e835f8159c4facb4c7f55a86ae11b5c6
meroglum <45095695+meroglum@users.noreply.github.com>
2023-03-20 21:05:49 +0300

    Selenium Intro

---
+++ Selenium Intro/2023.02.27SeleniumFirstProject/Form1.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+//Lecture Notes
+namespace _2023._02._27SeleniumFirstProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            IWebDriver driver= new ChromeDriver();
+            driver.Url = "https://www.google.com";
+            var searchBox = driver.FindElement(By.Name("q"));
+
+            if (searchBox != null)
+            {
+                Thread.Sleep(5000);
+                searchBox.SendKeys("Muhammed EROGLU");
+                searchBox.SendKeys(OpenQA.Selenium.Keys.Enter);
+                Thread.Sleep(5000);
+                driver.Quit();
+            }
+        }
+    }
+}
